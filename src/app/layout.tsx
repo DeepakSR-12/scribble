1f7a2c4aa33fc22094f96a506c60140330d2ad2e
DeepakSR-12 <deepakdeepakeng@gmail.com>
2023-11-17 21:12:07 +0530

    feat: Scribble - Landing page

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+export const dynamic = "force-dynamic";
+
+import type { Metadata } from "next";
+import "./globals.css";
+import { ThemeProvider } from "@/lib/providers/next-theme-provider";
+import { DM_Sans } from "next/font/google";
+import { twMerge } from "tailwind-merge";
+
+const inter = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Scribble",
+  description: "All-In-One Collaboration and Productivity Platform",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={twMerge("bg-background", inter.className)}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
